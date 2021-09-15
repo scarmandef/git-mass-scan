cfd836b72552e7f7f2fd0fa8fa8ba2e9e058ecb4
scarmandef <80011252+scarmandef@users.noreply.github.com>
2021-09-15 01:29:39 -0300

    Create git_scan.py

---
+++ git_scan.py
@@ -0,0 +1,72 @@
+import requests
+import argparse
+
+VERMELHO = "\033[1;31m"
+VERDE = "\033[92m"
+BRANCO = "\033[0;0m"
+AMARELO = "\033[1;93m"
+CLARO = "\033[93m"
+
+
+parser = argparse.ArgumentParser(usage='gitscan.py -h for help -t hosts.txt -f git or env -o results.txt', description='mass scan for .GIT and .ENV repository exposure')
+parser.add_argument('-t', help= VERDE + 'Load the file with the targets. '
+                             + CLARO +  'Example: hosts.txt || /home/user/Desktop/hosts.txt' + BRANCO, required=True)
+parser.add_argument('-f', help= VERDE + 'Choose git or env function' + BRANCO, required=True)
+parser.add_argument('-o', help= VERDE + 'Place the file to be saved.'
+                              + CLARO + '\n' + ' Example: Resultados.txt || /home/user/Desktop/Resultados.txt' + BRANCO, required=True)
+args = parser.parse_args()
+hosts = str(args.t)
+funcao = str(args.f)
+output = str(args.o)
+
+try:
+    files = open(hosts, 'r', encoding="utf8")
+except FileNotFoundError as e:
+    print('\n',hosts,'not found, please rate the file name or directory.')
+    exit()
+
+resultado = open(output, 'w', encoding="utf8")
+
+header = {'User-Agent':
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'
+          }
+
+
+def env(file):
+    for site in files:
+        site = site.rstrip()
+        try:
+            env = requests.get(site + '/.env', headers=header)
+            status_env = env.status_code
+            if status_env == 200:
+                resultado.writelines(site + ' - ENV' + '\n')
+                print(site, VERDE + '[+] Env found ! [+]' + BRANCO)
+            else:
+                print(site, VERMELHO + '[-] Env not found ! [-]' + BRANCO)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError) as e:
+            print(site, AMARELO + '[-] Invalid url or site down [-]' + BRANCO)
+
+
+def git(file):
+    for site in files:
+        site = site.rstrip()
+        try:
+            git = requests.get(site + '/.git/HEAD', headers=header)
+            status_git = git.status_code
+            if status_git == 200:
+                resultado.writelines(site + ' - GIT ' + '\n')
+                print(site, VERDE + '[+] Git found ! [+]' + BRANCO)
+            else:
+                print(site, VERMELHO + '[-] Git not found ! [-]' + BRANCO)
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError) as e:
+            print(site, AMARELO + '[-] Invalid url or site down [-]' + BRANCO)
+
+if funcao == 'git':
+     git(hosts)
+elif funcao == 'env':
+     env(hosts)
+else:
+    print(VERMELHO + '[-] Invalid function [-]' + BRANCO)
+
+resultado.close()
+files.close()
