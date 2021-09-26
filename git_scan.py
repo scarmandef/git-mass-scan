48cee6bf7ce61f605c149c20a05ceff532cdd9bd
scarmandef <80011252+scarmandef@users.noreply.github.com>
2021-09-25 21:09:51 -0300

    Update git_scan.py

--- git_scan.py
+++ git_scan.py
@@ -35,8 +35,6 @@ header = {'User-Agent':
             'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'
           }
 
-erros = ''
-
 
 def env(file):
     for site in files:
